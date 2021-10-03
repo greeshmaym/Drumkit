d78069f30f6c33dbc55e743ca9ab44ef7e284c1a
greeshmaym <greeshmaym@gmail.com>
2021-10-03 13:20:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    clickFun(this.innerHTML);
+    ani(this.innerHTML);
+  });
+document.querySelectorAll(".drum")[i].addEventListener("keydown",function(event){
+    clickFun(event.key);
+});
+
+
+
+}
+
+function clickFun(value) {
+  switch (value) {
+    case "w":
+      audio = new Audio("./sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      audio = new Audio("./sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      audio = new Audio("./sounds/snare.mp3");
+      audio.play();
+      break;
+    case "d":
+      audio = new Audio("./sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      audio = new Audio("./sounds/tom-2.mp3");
+      audio.play();
+      break;
+
+    case "k":
+      audio = new Audio("./sounds/tom-3.mp3");
+      audio.play();
+      break;
+
+    case "l":
+      audio = new Audio("./sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    default:
+      break;
+  }
+}
+
+function ani(val)
+{
+    document.querySelector("."+val).classList.add("pressed");
+    setTimeout( function (){document.querySelector("."+val).classList.remove("pressed");} 
+        
+    , 100);
+}
